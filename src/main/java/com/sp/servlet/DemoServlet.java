030f6f13a90635747325a7ab9b669db3c96a0f7d
shubham12333 <shubhamparmar3300@gmail.com>
2024-10-25 16:48:07 +0530

    Functionalities done in DemoServlet

---
+++ src/main/java/com/sp/servlet/DemoServlet.java
@@ -0,0 +1,9 @@
+package com.sp.servlet;
+
+public class DemoServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req , HttpServletResponse resp)throws ServletException,IOException
+	{
+			
+	}
+}
